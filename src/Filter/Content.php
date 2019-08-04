3d08de0ddba594ef5b4b6796bfd0ed64cec7ce54
sters <sters@users.noreply.github.com>
2019-08-05 00:14:13 +0900

    fix content filter

--- src/Filter/Content.php
+++ src/Filter/Content.php
@@ -20,9 +20,11 @@ class Content extends Filter
 
     public function add()
     {
+        $this->name = 'the_title';
         $this->func = $this->titleHook;
         parent::add();
 
+        $this->name = 'the_content';
         $this->func = $this->bodyHook;
         parent::add();
     }
