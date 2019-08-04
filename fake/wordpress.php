bafbe089e52705fb4fcdeb6ff68f580c3f851808
sters <sters@users.noreply.github.com>
2019-08-04 15:23:59 +0900

    x

--- fake/wordpress.php
+++ fake/wordpress.php
@@ -1,8 +1,22 @@
 <?php
 
-class WP_Query{
-    public function __construct($a){}
+class WP_Query
+{
+    public function __construct($a)
+    { }
 }
+class WP_REST_Request
+{ }
 
-function wp_get_post_tags($a, $b) {}
-
+function wp_get_post_tags($a, $b)
+{ }
+function get_permalink($a)
+{ }
+function get_the_author_meta($a, $b)
+{ }
+function get_template_directory_uri()
+{ }
+function add_action($a, $b)
+{ }
+function register_rest_route($a, $b, $c)
+{ }
