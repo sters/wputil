63e54effafd45263ec3eb23dfb1eafb048ff276f
sters <sters@users.noreply.github.com>
2019-08-04 23:01:16 +0900

    update enquee

--- src/Filter/EnqueueScript.php
+++ src/Filter/EnqueueScript.php
@@ -3,15 +3,13 @@ namespace WPUtil\Filter;
 
 class EnqueueScript extends Filter
 {
-    public function __construct($deregisters, $dequeues)
+    public function __construct($deregisters)
     {
         $this->name = 'wp_enqueue_scripts';
-        $this->func = function () use ($deregisters, $dequeues) {
+        $this->func = function () use ($deregisters) {
             foreach ($deregisters as $deregister) {
                 wp_deregister_script($deregister);
                 wp_deregister_style($deregister);
-            }
-            foreach ($dequeues as $dequeue) {
                 wp_dequeue_script($dequeue);
                 wp_dequeue_style($dequeue);
             }
