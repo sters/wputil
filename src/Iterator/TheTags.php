6f25c01188e70689bf4c4a68a8340d1bd32e3cc0
sters <sters@users.noreply.github.com>
2019-08-28 11:24:06 +0900

    fix

--- src/Iterator/TheTags.php
+++ src/Iterator/TheTags.php
@@ -5,6 +5,6 @@ class TheTags extends WpItems
 {
     public function __construct($post)
     {
-        $this->items = get_the_terms($post, 'post_tag')
+        $this->items = get_the_terms($post, 'post_tag');
     }
 }
